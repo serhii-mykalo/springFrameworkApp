a74a59b6cd6f8a768db4a40995a349c05672abd1
Serhii <serhii.mask@gmail.com>
2023-12-17 20:08:27 +0200

    feat: add computer.java

---
+++ src/main/java/org/example/Computer.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Computer {
+    private int id;
+    private MusicPlayer musicPlayer;
+
+    @Autowired
+    public Computer(MusicPlayer musicPlayer) {
+        this.id = 1;
+        this.musicPlayer = musicPlayer;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "id=" + id +
+                ", musicPlayer=" + musicPlayer +
+                '}';
+    }
+}
--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
 
+    @Autowired
     private Music music;
 
     public MusicPlayer() {}
