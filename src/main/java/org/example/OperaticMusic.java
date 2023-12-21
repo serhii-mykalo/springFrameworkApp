b84b7fa8cd8a86dc2d444ea48d6cfb049e8e70b1
Serhii <serhii.mask@gmail.com>
2023-12-21 20:52:29 +0200

    feat: edit OperaticMusic

--- src/main/java/org/example/OperaticMusic.java
+++ src/main/java/org/example/OperaticMusic.java
@@ -2,10 +2,22 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class OperaticMusic implements Music {
+
+    private List<String> songs = new ArrayList<>();
+
+    {
+        songs.add("Some Operatic music one");
+        songs.add("Some Operatic music two");
+        songs.add("Some Operatic music three");
+    }
+
     @Override
-    public String getSong() {
-        return "Some famous Operatic song";
+    public List<String> getSongs() {
+        return songs;
     }
 }
